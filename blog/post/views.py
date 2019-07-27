3330d4c66683763e918bc9130f958d27e1a4929a
ulysses316 <ulises316@live.com.mx>
2019-07-27 17:23:41 -0500

    se hacen los templates dinamicos

--- blog/post/views.py
+++ blog/post/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from post.models import Post
 
 # Create your views here.
+
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, 'post/post_list.html', {'posts': posts})
